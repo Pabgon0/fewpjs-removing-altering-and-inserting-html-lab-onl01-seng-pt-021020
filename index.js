bb346c28f896478f520a9276bf1010af34167fc8
Pablo Gonzalez <pabgon0@gmail.com>
2020-08-12 20:47:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ document.getElementById("main").remove()
 const newHeader = document.createElement("h1")
 newHeader.classname = "victory"
 newHeader.innerHTML = "Pablo Gonzalez is the champion"
+newHeader.id = "victory"
